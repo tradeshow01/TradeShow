d163a4f18f4ef2b7fbfa43f25b17d75863f07d50
tradeshow01 <110342169+tradeshow01@users.noreply.github.com>
2022-08-08 16:58:26 +0530

    a
    a

--- head.php
+++ head.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="shortcut icon" href="favicon.png" />
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com/">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
