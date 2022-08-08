5f02918013e60584df667fb1a033788e15b8f3ed
RahulWDC <vision.wdc@gmail.com>
2022-08-08 23:00:39 +0530

    AA
    A

--- footer.php
+++ footer.php
@@ -1,68 +1,99 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!--=================================
-Footer -->
-<footer class="iq-footer dark-bg iq-footer-2">
-    <div class="footer-top iq-bg-fixed ">
+<footer class="iq-footer-3 dark-bg">
+    <div class="footer-top overview-block-pt iq-pb-60">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12 iq-mtb-90">
+                <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="logo">
                         <img id="logo_img_2" class="img-fluid" src="images/logo-white.png" alt="# ">
-                        <div class="text-white iq-mt-25 ">With Tradeshoww, you can buy, sell, trade, and earn
-                            cryptocurrencies and manage your Transaction most efficiently and securely.</div>
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-offset-3 col-md-6 col-sm-12 iq-contact iq-mtb-90 footer-menu-list">
-                    <h5 class="small-title text-white widget-title">Services</h5>
-                    <div class="d-flex align-items-center text-white flex-wrap">
-                        <ul class="iq-pl-0 ">
-                            <li><a href="about-us.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Forex Trading</a></li>
-                            <li><a href="services-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Comex Trading</a></li>
-                            <li><a href="faqs-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Index Trading</a></li>
-                            <li><a href="team-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Team Trading</a></li>
-                            <li><a href="contact-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Crypto Trading</a></li>
+                        <div class="text-white iq-mt-15">Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry lorem Ipsum has been the industry's standard dummy.</div>
+                        <ul class="iq-media-blog iq-mt-20">
+                            <li><a href="# "><i class="fa fa-twitter "></i></a></li>
+                            <li><a href="# "><i class="fa fa-facebook "></i></a></li>
+                            <li><a href="# "><i class="fa fa-google "></i></a></li>
+                            <li><a href="# "><i class="fa fa-github "></i></a></li>
                         </ul>
-
                     </div>
                 </div>
-
-                <div class="col-lg-2 col-md-6 col-sm-12 iq-contact iq-mtb-90  footer-menu-list">
-                    <h5 class="small-title text-white widget-title">Important Links</h5>
-                    <div class="d-flex align-items-center text-white flex-wrap">
-                        <ul class="iq-pl-0 ">
-                            <li><a href="about-us.html"><i class="fa fa-caret-right" aria-hidden="true"></i>About Us</a></li>
-                            <li><a href="services-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Testimonial</a></li>
-                            <li><a href="faqs-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Payment</a></li>
-                            <li><a href="team-1.html"><i class="fa fa-caret-right" aria-hidden="true"></i>Contact Us</a></li>
+                <div class="col-lg-4 col-md-6 col-sm-12 iq-contact iq-r4-mt-40">
+                    <div class="heading-left">
+                        <h5 class="title  text-white">Contact CoinEx</h5>
+                    </div>
+                    <div class="iq-mb-20">
+                        <div class="blog"><i class="ion-ios-telephone-outline"></i>
+                            <div class="content">
+                                <div class=" title ">Phone</div> +0123 456 789
+                            </div>
+                        </div>
+                    </div>
+                    <div class="iq-mb-20">
+                        <div class="blog "><i class="ion-ios-email-outline"></i>
+                            <div class="content">
+                                <div class=" title ">Mail</div> mail@coinex.com
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog"><i class="ion-ios-location-outline"></i>
+                        <div class="content">
+                            <div class=" title ">Address</div> 1234 North Luke Lane, South Bend,IN 360001
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 col-sm-12 iq-r-mt-40">
+                    <div class="footer-menu">
+                        <div class="heading-left">
+                            <h5 class="title  text-white">Menu</h5>
+                        </div>
+                        <ul class="iq-pl-0">
+                            <li><a href="# ">Home</a></li>
+                            <li><a href="# ">About Us</a></li>
+                            <li><a href="# ">Services</a></li>
+                            <li><a href="# ">Faqs</a></li>
+                            <li><a href="# ">Contact Us</a></li>
                         </ul>
-
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 text-white iq-contact iq-mtb-90">
-                    <h5 class="small-title widget-title">Modal</h5>
-                    <img src="tradinggif.gif" class="img-fluid" />
+                <div class="col-lg-3 col-md-6 col-sm-12 iq-r-mt-40">
+                    <div class=" heading-left">
+                        <h5 class="title  text-white">Newsletter</h5>
+                    </div>
+                    <p class="text-white">Lorem Ipsum passages, and more recently with desktop publishing software
+                        like Aldus PageMaker including versions.</p>
+                    <form class="form-inline iq-subscribe mt-2">
+                        <div class="form-group">
+                            <div class="input-group mb-3 mt-4 rounded">
+                                <input type="text" class="form-control" placeholder="Enter your Email">
+                                <a class="button rounded-end" href="javascript:void(0)">
+                                    <span class="button-inner-box">
+                                        <span class="icon-one icon">
+                                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                                                <path fill="currentColor" d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z">
+                                                </path>
+                                            </svg></span>
+                                        <span class="text-btn">Go</span>
+                                        <span class="icon-two icon">
+                                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                                                <path fill="currentColor" d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z">
+                                                </path>
+                                            </svg></span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
             </div>
         </div>
     </div>
     <div class="footer-bottom iq-ptb-20 ">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-12 text-center">
                     <div class="iq-copyright iq-mt-10 text-white">Copyright <span id="copyright">
                             <script>
                                 document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
                             </script>
-                        </span> <a href="javascript:void(0)">Tradeshoww</a> All Rights Reserved </div>
-                </div>
-                <div class="col-sm-6">
-                    <ul class="footer-nav-menu">
-                        <li class=""><a class="text-white" href="about.html/index.html">About Us</a></li>
-                        <li class=""><a class="text-white" href="#">Privacy policy</a></li>
-                        <li class=""><a class="text-white" href="#/">Customer</a></li>
-                    </ul>
+                        </span> <a href="javascript:void(0)">CoinEx</a> All Rights Reserved </div>
                 </div>
             </div>
         </div>
@@ -148,8 +179,8 @@ Register -->
                     <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
                         printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <a class="button iq-mtb-10" href="javascript:void(0)">
-                        <span class="button-inner-box">
-                            <span>Sign Up</span>
+                        <span class="button-inner-box"></span>
+                        <span>Sign Up</span>
                         </span>
                     </a>
                 </form>
@@ -174,7 +205,28 @@ Register -->
 </div>
 <!-- back-to-top End -->
 <!-- style-customizer -->
-
+<div class="iq-customizer closed">
+    <div class="buy-button"> <a class="opener" href="javascript:void(0)"><i class="fa fa-spinner fa-spin"></i></a>
+    </div>
+    <div class="clearfix content-chooser">
+        <h3 class="iq-font-black">CoinEX Awesome Color</h3>
+        <p>This color combo available inside whole template. You can change on your wish, Even you can create your
+            own with limitless possibilities! </p>
+        <ul class="iq-colorChange clearfix">
+            <li class="color-1 selected" data-style="color-1"></li>
+            <li class="color-2" data-style="color-2"></li>
+            <li class="color-3" data-style="color-3"></li>
+            <li class="color-4" data-style="color-4"></li>
+            <li class="color-5" data-style="color-5"></li>
+            <li class="color-6" data-style="color-6"></li>
+            <li class="color-7" data-style="color-7"></li>
+            <li class="color-8" data-style="color-8"></li>
+            <li class="color-9" data-style="color-9"></li>
+            <li class="color-10" data-style="color-10"></li>
+        </ul>
+        <a target="_blank" class="button" href="javascript:void(0)">Purchase Now</a>
+    </div>
+</div>
 <!-- style-customizer END -->
 <!-- jquery-min JavaScript -->
 <script src="js/jquery-min.js"></script>
@@ -186,26 +238,94 @@ Register -->
 <script src="js/widget.js"></script>
 <!-- carousel JavaScript -->
 <script src="js/owl.carousel.min.js"></script>
-<!-- apex chart JavaScript -->
-<script src="js/apexcharts.min.js"></script>
 <!-- All-plugins JavaScript -->
 <script src="js/all-plugins.js"></script>
 <!-- particles JavaScript -->
 <script src="js/particles.js"></script>
 <!-- Style Customizer -->
 <script src="js/style-customizer.js"></script>
-
+<!-- REVOLUTION JS FILES -->
+<script src="revslider/js/jquery.themepunch.tools.min.js"></script>
+<script src="revslider/js/jquery.themepunch.revolution.min.js"></script>
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="revslider/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="revslider/js/extensions/revolution.extension.video.min.js"></script>
 <!-- Custom JavaScript -->
-
-<!-- <script src="swiper/swiper.css"></script> -->
 <script src="js/custom.js"></script>
-
-<!-- Gsap animation -->
-<!-- Initialize Swiper -->
-<script src="swiper/swiper.js"></script>
-
+<script>
+    var revapi4,
+        tpj = jQuery;
+    tpj(document).ready(function() {
+        if (tpj("#rev_slider_4_1").revolution == undefined) {
+            revslider_showDoubleJqueryError("#rev_slider_4_1");
+        } else {
+            revapi4 = tpj("#rev_slider_4_1").show().revolution({
+                sliderType: "standard",
+                sliderLayout: "fullwidth",
+                dottedOverlay: "none",
+                delay: 9000,
+                navigation: {
+                    keyboardNavigation: "off",
+                    keyboard_direction: "horizontal",
+                    mouseScrollNavigation: "off",
+                    mouseScrollReverse: "default",
+                    onHoverStop: "off",
+                    arrows: {
+                        style: "zeus",
+                        enable: true,
+                        hide_onmobile: false,
+                        hide_onleave: false,
+                        tmp: '<div class="tp-title-wrap">    <div class="tp-arr-imgholder"></div> </div>',
+                        left: {
+                            h_align: "left",
+                            v_align: "center",
+                            h_offset: 20,
+                            v_offset: 0
+                        },
+                        right: {
+                            h_align: "right",
+                            v_align: "center",
+                            h_offset: 20,
+                            v_offset: 0
+                        }
+                    }
+                },
+                visibilityLevels: [1240, 1024, 778, 480],
+                gridwidth: 1170,
+                gridheight: 790,
+                lazyType: "none",
+                shadow: 0,
+                spinner: "spinner0",
+                stopLoop: "off",
+                stopAfterLoops: -1,
+                stopAtSlide: -1,
+                shuffle: "off",
+                autoHeight: "off",
+                disableProgressBar: "on",
+                hideThumbsOnMobile: "off",
+                hideSliderAtLimit: 0,
+                hideCaptionAtLimit: 0,
+                hideAllCaptionAtLilmit: 0,
+                debugMode: false,
+                fallbacks: {
+                    simplifyAll: "off",
+                    nextSlideOnWindowFocus: "off",
+                    disableFocusListener: false,
+                }
+            });
+        }
+    }); /*ready*/
+</script>
 </body>
 
 
+<!-- Mirrored from templates.iqonic.design/coinex/html/dark/home-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Jul 2022 17:58:48 GMT -->
 
 </html>
