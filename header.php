a7720ccf6a18af310fa186e945cb9c093308a91a
tradeshow01 <110342169+tradeshow01@users.noreply.github.com>
2022-08-08 17:19:22 +0530

    s
    s

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <!-- loading -->
     <div id="loading">
         <div id="loading-center">
-            <img src="favicon.png" alt="loder">
+            <img src="logo3.png" alt="loder">
         </div>
     </div>
     <!-- loading End -->
