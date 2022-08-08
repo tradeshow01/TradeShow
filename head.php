2cb6cb803664da16b0e44167acc231681318dc38
RahulWDC <vision.wdc@gmail.com>
2022-08-09 00:25:40 +0530

    a

--- head.php
+++ head.php
@@ -8,31 +8,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
     <link rel="shortcut icon" href="favicon.png" />
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;1,100;1,200;1,300&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <!-- Typography CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
     <link rel="stylesheet" href="css/typography.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- style CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Responsive CSS -->
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
-    <!-- apexchart CSS -->
-    <link rel="stylesheet" type="text/css" href="css/apexcharts.css">
     <!-- Style customizer (Remove these two lines please) -->
     <link rel="stylesheet" href="javascript:void(0)" data-style="styles">
     <link rel="stylesheet" href="css/style-customizer.css" />
+    <!-- gsap -->
+    <script src="gsap/gsap.min.js"></script>
+    <!-- gsap -->
     <!-- swiper -->
-    <link rel="stylesheet" href="swiper/swiper.css">
-    <link rel="stylesheet" href="swiper/swiper.min.css">
+    <link rel="stylesheet" href="./swiper/swiper.css">
+    <link rel="stylesheet" href="./swiper/swiper.min.css">
+    <script src="./swiper/swiper.min.js"></script>
     <script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
     <script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
