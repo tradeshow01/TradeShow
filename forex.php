ac4a811352a1e0338de406817fe0dc1b58a0cc64
tradeshow01 <110342169+tradeshow01@users.noreply.github.com>
2022-08-29 17:51:58 +0530

    a
    a

--- forex.php
+++ forex.php
@@ -78,18 +78,19 @@ include("header.php");
                     <p class="iq-mt-25">Forex, or foreign exchange, can be explained as a network of buyers and sellers, who transfer currency between each other at an agreed price. It is the means by which individuals, companies and central banks convert one currency into another – if you have ever travelled abroad, then it is likely you have made a forex transaction.
 
                     </p>
+                    <p class="iq-mt-25">While a lot of foreign exchange is done for practical purposes, the vast majority of currency conversion is undertaken with the aim of earning a profit. The amount of currency converted every day can make price movements of some currencies extremely volatile. It is this volatility that can make forex so attractive to traders: bringing about a greater chance of high profits, while also increasing the risk.</p>
                     <a class="button iq-mt-30" href="javascript:void(0)"><span class="button-inner-box">
                             <span class="icon-one icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
                                     <path fill="currentColor" d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z"></path>
                                 </svg></span>
-                            <span class="text-btn">Read More</span>
                             <span class="icon-two icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
                                     <path fill="currentColor" d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z"></path>
                                 </svg></span>
                         </span></a>
                 </div>
             </div>
-            <p>It refers to Foreign Exchange Market. This is the market which is completely decentralized and where currencies are traded. FOREX is also known as FX is global market which allows us to trade two currencies against each other. Foreign Exchange market is the largest financial market in the world.</p>
+            <p>How FX Trading works
+Trading forex involves the buying of one currency and simultaneous selling of another. In forex, traders attempt to profit by buying and selling currencies by actively speculating on the direction currencies are likely to take in the future.</p>
         </div>
     </section>
 
